f6754242466c3f54460f0a04a6b6a21b8724d30b
Vero-Alvaro <85202315+Vero-Alvaro@users.noreply.github.com>
2021-07-15 13:35:47 -0500

    Update service.py

--- script/service.py
+++ script/service.py
@@ -14,6 +14,8 @@ import json
 import os
 from werkzeug.utils import secure_filename
 from model_loader import cargarModelo
+from skimage.metrics import structural_similarity as ssim
+import matplotlib.pyplot as plt
 
 UPLOAD_FOLDER = '../images/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -67,41 +69,108 @@ def default():
             test_image = test_image/255.
             test_image = cv2.resize(test_image, (WIDTH,HEIGHT))
             test_image = test_image.reshape(-1,WIDTH,HEIGHT,1)
-        
-            result = loaded_model.predict(test_image)
+            
+            
+            def mse(imageA, imageB):
+            # the 'Mean Squared Error' between the two images is the
+            # sum of the squared difference between the two images;
+            # NOTE: the two images must have the same dimension
+            err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+            err /= float(imageA.shape[0] * imageA.shape[1])
+            # return the MSE, the lower the error, the more "similar"
+            # the two images are
+            return err
+
+            def compare_images(imageA, imageB, title):
+              # compute the mean squared error and structural similarity
+              # index for the images
+              m = mse(imageA, imageB)
+              s = ssim(imageA, imageB)
+              # setup the figure
+              fig = plt.figure(title)
+              plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+              # show first image
+              ax = fig.add_subplot(1, 2, 1)
+              plt.imshow(imageA, cmap = plt.cm.gray)
+              plt.axis("off")
+              # show the second image
+              ax = fig.add_subplot(1, 2, 2)
+              plt.imshow(imageB, cmap = plt.cm.gray)
+              plt.axis("off")
+              # show the images
+              plt.show() 
+
+              if(s<0.75):
+                print("La imagen no coincide con una radiografia de torax, incerte otra imagen")
+              
+            # load the images -- the original, the original + contrast,
+            # and the original + photoshop
+            original = cv2.imread("comparacion/abuelo.jpg")
+            contrast = cv2.imread(test_image)
+            #contrast = cv2.imread("comparacion/abuelo.jpg")
+
+            # convert the images to grayscale and resize 64x64
+            WIDTH=256
+            HEIGHT=256
+
+            original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+            original= cv2.resize(original, (WIDTH,HEIGHT))
+            contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
+            contrast= cv2.resize(contrast, (WIDTH,HEIGHT))
+
+
+            # initialize the figure
+            fig = plt.figure("Images")
+            images = ("Original", original), ("Contrast", contrast)
+            # loop over the images
+            for (i, (name, image)) in enumerate(images):
+              # show the image
+              ax = fig.add_subplot(1, 3, i + 1)
+              ax.set_title(name)
+              plt.imshow(image, cmap = plt.cm.gray)
+              plt.axis("off")
+            # show the figure
+            plt.show()
+            # compare the images
+            compare_images(original, contrast, "Original vs. Contrast")
+
+            
+            
+              else:
+                result = loaded_model.predict(test_image)
             	# print(result)
             	
-            # Resultados
-            if(result[0][0]>result[0][1] and result[0][0]>result[0][2]): 
-                prediction = 0
+                # Resultados
+                if(result[0][0]>result[0][1] and result[0][0]>result[0][2]): 
+                    prediction = 0
 
-            elif(result[0][1]>result[0][0] and result[0][1]>result[0][2]): 
-                prediction = 1    
+                elif(result[0][1]>result[0][0] and result[0][1]>result[0][2]): 
+                    prediction = 1    
 
-            else: prediction = 2
-                
-            CLASSES = ["NORMAL", "COVID-19", "Viral Pneumonia"]
-            ClassPred = CLASSES[prediction]
+                else: prediction = 2
 
-            # print("Pedicción:", ClassPred)
-            # print("Prob:", "{0:.2f}".format(ClassProb))
+                CLASSES = ["NORMAL", "COVID-19", "Viral Pneumonia"]
+                ClassPred = CLASSES[prediction]
 
-            #Results as Json
-            data["predictions"] = []
-            if(prediction == 0):
-                
-                r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,0]*100)}
-            elif(prediction == 1):
-                r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,1]*100)}
-            else:
-                r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,2]*100)}
-                
-            data["predictions"].append(r)
+                # print("Pedicción:", ClassPred)
+                # print("Prob:", "{0:.2f}".format(ClassProb))
 
-            #Success
-            data["success"] = True
+                #Results as Json
+                data["predictions"] = []
+                if(prediction == 0):
 
-    return jsonify(data)
+                    r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,0]*100)}
+                elif(prediction == 1):
+                    r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,1]*100)}
+                else:
+                    r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,2]*100)}
+
+                data["predictions"].append(r)
+
+                #Success
+                data["success"] = True
+
+        return jsonify(data)
 
 # Run de application
 app.run(host='0.0.0.0',port=port, threaded=False)
