7bfa56906f1ed21f057f246428c6ed2211a5d315
Vero-Alvaro <85202315+Vero-Alvaro@users.noreply.github.com>
2021-07-16 21:53:38 -0500

    Update service.py

--- script/service.py
+++ script/service.py
@@ -14,8 +14,6 @@ import json
 import os
 from werkzeug.utils import secure_filename
 from model_loader import cargarModelo
-from skimage.metrics import structural_similarity as ssim
-import matplotlib.pyplot as plt
 
 UPLOAD_FOLDER = '../images/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -57,11 +55,6 @@ def default():
             filename = UPLOAD_FOLDER + '/' + filename
             print("\nfilename:",filename)
             
-            #image_to_predict = image.load_img(filename, target_size=(64, 64))
-            #test_image = image.img_to_array(image_to_predict)
-            #test_image = np.expand_dims(test_image, axis = 0)
-            #test_image = test_image.astype('float32')
-            #test_image /= 255
             WIDTH=64
             HEIGHT=64
 
@@ -69,31 +62,24 @@ def default():
             test_image = test_image/255.
             test_image = cv2.resize(test_image, (WIDTH,HEIGHT))
             test_image = test_image.reshape(-1,WIDTH,HEIGHT,1)
-            result = loaded_model.predict(test_image)
 
-            
-            def compare_images(imageA, imageB, title):
-              # compute the mean squared error and structural similarity
-              # index for the images
-              s = ssim(imageA, imageB)
-              # setup the figure
-              fig = plt.figure(title)
-              plt.suptitle("SSIM: %.2f" % (s))
-              # show first image
-              ax = fig.add_subplot(1, 2, 1)
-              plt.imshow(imageA, cmap = plt.cm.gray)
-              plt.axis("off")
-              # show the second image
-              ax = fig.add_subplot(1, 2, 2)
-              plt.imshow(imageB, cmap = plt.cm.gray)
-              plt.axis("off")
-              # show the images
-              plt.show() 
+            original = cv2.imread("abuelo.jpg")
+            contrast = cv2.imread(test_image)
 
-              if(s<0.75):
-                print("La imagen no coincide con una radiografia de torax, incerte otra imagen")
+	    WIDTH=256
+            HEIGHT=256
+
+	    original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+            contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
+	    original= cv2.resize(original, (WIDTH,HEIGHT))
+	    contrast= cv2.resize(contrast, (WIDTH,HEIGHT))
+	    s=ssim(original, contrast)
+
+            if(s<0.75):
+		print("Esta imagen no coincide con una raiografía de torax, por favor elija otra")
 		
-              else:
+	    else:		
+                result = loaded_model.predict(test_image)
             	# print(result)
             	
                 # Resultados
@@ -104,7 +90,7 @@ def default():
                     prediction = 1    
 
                 else: prediction = 2
-
+                
                 CLASSES = ["NORMAL", "COVID-19", "Viral Pneumonia"]
                 ClassPred = CLASSES[prediction]
 
@@ -114,55 +100,19 @@ def default():
                 #Results as Json
                 data["predictions"] = []
                 if(prediction == 0):
-
+                
                     r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,0]*100)}
                 elif(prediction == 1):
                     r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,1]*100)}
                 else:
                     r = {"label": ClassPred, "score": "{0:.2f}".format(result[0,2]*100)}
-
+                
                 data["predictions"].append(r)
 
                 #Success
                 data["success"] = True
 
-#        return jsonify(data)
-
-            # load the images -- the original, the original + contrast,
-            # and the original + photoshop
-            original = cv2.imread("abuelo.jpg")
-            contrast = cv2.imread(test_image)
-            #contrast = cv2.imread("comparacion/abuelo.jpg")
-
-            # convert the images to grayscale and resize 64x64
-            WIDTH=256
-            HEIGHT=256
-
-            original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-            original= cv2.resize(original, (WIDTH,HEIGHT))
-            contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
-            contrast= cv2.resize(contrast, (WIDTH,HEIGHT))
-
-
-            # initialize the figure
-            fig = plt.figure("Images")
-            images = ("Original", original), ("Contrast", contrast)
-            # loop over the images
-            for (i, (name, image)) in enumerate(images):
-              # show the image
-              ax = fig.add_subplot(1, 3, i + 1)
-              ax.set_title(name)
-              plt.imshow(image, cmap = plt.cm.gray)
-              plt.axis("off")
-            # show the figure
-            plt.show()
-            # compare the images
-            compare_images(original, contrast, "Original vs. Contrast")
-            
-         
-
-
-        return jsonify(data)
+    return jsonify(data)
 
 # Run de application
 app.run(host='0.0.0.0',port=port, threaded=False)
